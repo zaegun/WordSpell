319ec01ba0d6c04d1f57610916645529e48df592
zaegun <gdbarker@gmail.com>
2022-02-16 12:36:22 -0800

    Initial Singleton test done

--- app/src/main/java/com/example/wordspell/MainMenu.kt
+++ app/src/main/java/com/example/wordspell/MainMenu.kt
@@ -2,10 +2,13 @@ package com.example.wordspell
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainMenu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
+        var startUpText : TextView = findViewById<TextView>(R.id.main_menu_header)
+        startUpText.text = WordData.name
     }
 }
--- app/src/main/java/com/example/wordspell/Startup.kt
+++ app/src/main/java/com/example/wordspell/Startup.kt
@@ -2,10 +2,34 @@ package com.example.wordspell
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Button
+import android.content.Intent
 
 class Startup : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_startup)
+
+        WordData.setName("Billy")
+        val startUpText : TextView = findViewById<TextView>(R.id.startUpLabel)
+        startUpText.text = WordData.name
+
+        val button : Button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            val intent = Intent(this@Startup, MainMenu::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
+
+object WordData {
+    var name = ""
+
+    // Sets the Singleton name for testing
+    @JvmName("setName1")
+    fun setName(newName : String) {
+        name = newName
     }
 }
