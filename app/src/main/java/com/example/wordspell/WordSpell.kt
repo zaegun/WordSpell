8fd82a5db06c28936f78ebd68f3255906be4bf01
LloydExe857 <alonghurst571@gmail.com>
2022-03-10 20:36:06 -0700

    Reference notes in Word Spell

--- app/src/main/java/com/example/wordspell/WordSpell.kt
+++ app/src/main/java/com/example/wordspell/WordSpell.kt
@@ -10,5 +10,15 @@ class WordSpell : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_word_spell)
+
+//        // Save the total word list
+//        val wordList = Global.getTestList()
+          // Grab text box elements
+//        var textBox = findViewById<TextView>(R.id.test_box)
+//        // textBox.text = wordList[0]
+//        val apple = wordList[0]
+//        Global.setScore(apple, 2)
+//        var score = Global.getScore(apple)
+//        var track = Global.getTrackNum()
     }
 }
