4b118e9a213185e60c9bcc2c1de3f5f595084afd
LloydExe857 <alonghurst571@gmail.com>
2022-03-11 00:55:54 -0700

    Added some elements to the wordSpell class, added a code to loop through all the words, and made a makeshift ui for the wordspell activity.

--- app/src/main/java/com/example/wordspell/WordSpell.kt
+++ app/src/main/java/com/example/wordspell/WordSpell.kt
@@ -2,23 +2,47 @@ package com.example.wordspell
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
-import android.widget.Button
-import android.content.Intent
 
 class WordSpell : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_word_spell)
+    }
+    //        // Save the total word list
+    val wordList = Global.getTestList()
+    var currentWord = ""
+    val wordLetters = mutableListOf<String>()
+    val letterOptions = mutableListOf<String>()
+    val currentLetters = mutableListOf<String>()
 
-//        // Save the total word list
-//        val wordList = Global.getTestList()
-          // Grab text box elements
+    // Grab text box elements
 //        var textBox = findViewById<TextView>(R.id.test_box)
 //        // textBox.text = wordList[0]
 //        val apple = wordList[0]
 //        Global.setScore(apple, 2)
 //        var score = Global.getScore(apple)
 //        var track = Global.getTrackNum()
+
+    // To keep track of what word we are on and how many words we have
+    val numWords = wordList.size
+    private fun progressList() {
+        for (i in 0..numWords) {
+            currentWord = wordList[i]
+            currentWord.forEach { j -> wordLetters.add(j.toString()) }
+            initializePage(currentWord,letterOptions,wordLetters)
+            spellWord(letterOptions,wordLetters)
+        }
+    }
+
+    private fun spellWord(letterOptions: MutableList<String>, wordLetters: MutableList<String>) {
+        TODO("Not yet implemented")
+    }
+
+    private fun initializePage(
+        currentWord: String,
+        letterOptions: MutableList<String>,
+        wordLetters: MutableList<String>
+    ) {
+        TODO("Not yet implemented")
     }
 }
