74172e43e9edc23c6035486a05b41e9d082ffb3e
Adamlynn123 <Adamlynn123@gmail.com>
2022-03-11 09:37:57 -0700

    March 11 Commit

--- app/src/main/java/com/example/wordspell/Results.kt
+++ app/src/main/java/com/example/wordspell/Results.kt
@@ -10,6 +10,19 @@ class Results : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_results)
+
+
+        var testList = Global.getTestList()
+        var apple = testList[0]
+        var textBox = findViewById<TextView>(R.id.test_text_box)
+        var score = Global.getScore(apple)
+        var track = Global.getTrackNum()
+        textBox.text = apple.toString() + score.toString() + track.toString()
+
+//        create a loop to loop through each item in the list
+
+
+
     }
 }
 
