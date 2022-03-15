a71808d30573f983981737467141e3e71d7e5746
zaegun <gdbarker@gmail.com>
2022-03-15 15:02:29 -0700

    Started Testing

---
+++ app/src/main/java/com/example/wordspell/DBTest.kt
@@ -0,0 +1,20 @@
+package com.example.wordspell
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DBTest : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dbtest)
+    }
+
+    // Load Data list
+    // Get the JSON List
+    // Check if each of the words is in the Data list
+    // If not add the JSON word to the data list
+    // else move on
+
+
+
+}
