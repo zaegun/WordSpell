0c7bffd77d3a1de9ed1312d31fa844b8e5c58e5f
zaegun <gdbarker@gmail.com>
2022-03-15 17:00:49 -0700

    DB Loads Data, Checks it all, and can update scores

--- app/src/main/java/com/example/wordspell/Startup.kt
+++ app/src/main/java/com/example/wordspell/Startup.kt
@@ -1,5 +1,6 @@
 package com.example.wordspell
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
@@ -42,7 +43,7 @@ class Startup : AppCompatActivity() {
 
         val button4 = findViewById<Button>(R.id.button4)
         button4.setOnClickListener {
-            val intent = Intent(this@Startup, HighScore::class.java)
+            val intent = Intent(this@Startup, DBTest::class.java)
             startActivity(intent)
             finish()
         }
@@ -94,126 +95,5 @@ class Startup : AppCompatActivity() {
         startActivity(intent)
         finish()
     }
-}
-
-// Class for creating a word
-class WordData(newWord : String, newScore : Int, newDate : String ){
-    // The object contains the following pieces of data
-    var word = "default"        // Word value
-        private set             // Word can only be set on creation
-    var score = 0               // Score from 0-3
-        set(value) {      // Sets the score if between 0-3
-            field = if(value in 0..3) {value} else {0}
-        }
-    var scoreDate = "default"   // String that has the time and date
-
-    init {
-        // Set the word, score, and the recorded date of that score
-        word = newWord
-        score = newScore
-        scoreDate = newDate
-    }
-}
-
-object Global {
-    // GLOBAL DATA
-    private var wordData = mutableListOf<WordData>()    // Contains all word data
-    var wordList = mutableListOf<String>()              // List of all the words
-        private set             // Should only be set at startup
-
-    // The working list is an array of words that are created based on the number of words selected
-    var workingList = mutableListOf<String>()   // Smaller array of words for usage
-    var numberOfWords = 0       // The amount of words the user wants to go through
-    var resultsData = mutableListOf<WordData>()
-
-    fun setCurrentDataList(data : WordData) {
-        // Adds the word data to the list
-        wordData.add(data)
-    }
-
-    fun generateWordList(){
-        // Makes a list using the word names in the data
-        for(wordData in wordData) {
-            wordList.add(wordData.word)
-        }
-    }
-
-    fun getScore(refWord : String) : Int {
-        // This finds the data based on the reference word
-        val foundWord = wordData.find {it.word == refWord}
-        //  Return the score
-        return foundWord?.score!!
-    }
-
-    fun setScore(refWord : String, newScore : Int) {
-        // This updates the score and the date the score was changed
-        // This finds the data based on the reference word
-        val foundWord = wordData.find {it.word == refWord}
-        // First check if the the score needs to be updated
-        if (foundWord?.score != newScore) {
-            // If it needs updating, update with the new score and date
-            foundWord?.score = newScore
-        }
-    }
-
-    fun setWorkingList(word : String) {
-        // Adds the word to the working list
-        workingList.add(word)
-    }
-
-    fun resetWorkingList(){
-        // Resets the working list to a blank array
-        workingList.clear()
-    }
-
-    fun getTrackNum() : Int {
-        // This returns the number of words the user wants to be tested on
-        return numberOfWords
-    }
-
-    fun setTrackNum(amount : Int) {
-        // This sets the number of words the user wants to be tested on
-        numberOfWords = amount
-    }
-
-    fun resetTrackNum() {
-        // Sets the number of words counter to 0
-        setTrackNum(0)
-    }
-
-    fun getResultsData(list: MutableList<String>) : MutableList<WordData> {
-        // For the Results screen the Recycle View needs access to the word name and score.
-        // As the working list only contains the word names, we need to create a list with
-        // Word Data objects in them using the supplied working list without effecting
-        // The main data.
-
-        // Cycle through the supplied list
-        for (reference in list) {
-            // Find the correct word data object
-            val foundWord = wordData.find { it.word == reference }
-            // Add the object to the resultsData list
-            if (foundWord != null) {
-                resultsData.add(foundWord)
-            }
-        }
-        // Return the list which has the word data
-        return resultsData
-    }
-
-    fun getTestList(): MutableList<String> {
-        // Creates a list for testing, updates their scores, and applies the correct tracking number
-        val testList = mutableListOf<String>()
-        testList.add("apple")
-        testList.add("truck")
-        testList.add("school")
-
-        setTrackNum(testList.size)
-        for(word in testList) {
-            setScore(word, (0..3).random() )
-        }
-
-        return testList
-    }
-
 }
 
