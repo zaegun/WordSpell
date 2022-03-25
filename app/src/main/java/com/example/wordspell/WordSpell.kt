c7606e85d4b1c812abf8190b7bee03f559595b43
LloydExe857 <alonghurst571@gmail.com>
2022-03-24 23:04:10 -0600

    Work on getting spellWord function operational

--- app/src/main/java/com/example/wordspell/WordSpell.kt
+++ app/src/main/java/com/example/wordspell/WordSpell.kt
@@ -3,6 +3,8 @@ package com.example.wordspell
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class WordSpell : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,7 +14,7 @@ class WordSpell : AppCompatActivity() {
     // Save the total word list
     val wordList = Global.getTestList()
     // The word currently being spelled
-    var currentWord = ""
+    var currentWord = "apple"
     // Number of the current word
     var currentWordNum = 0
     // Letters of the current word
@@ -20,11 +22,12 @@ class WordSpell : AppCompatActivity() {
     // Letters to be chosen from
     val letterOptions = mutableListOf<String>()
     // Letters currently selected
-    val currentLetters = mutableListOf<String>()
+    var currentLetters = ""
     // Track the current letter in the word
     var currentLetterNum = 0
     // To keep track of what word we are on and how many words we have
     val numWords = wordList.size
+
 
     // Grab text box elements
 //        var textBox = findViewById<TextView>(R.id.test_box)
@@ -41,25 +44,74 @@ class WordSpell : AppCompatActivity() {
             currentWord = wordList[currentWordNum]
             // Clear the letter list and the selected letters
             wordLetters.clear()
-            currentLetters.clear()
+            currentLetters = ""
             // Reset letter count
             currentLetterNum = 0
             // Fill letter list with letters in current
-            currentWord.forEach { j -> wordLetters.add(j.toString()) }
+            //currentWord.forEach { j -> wordLetters.add(j.toString()) }
+            for (letter in currentWord)
+                wordLetters.add(letter.toString())
             initializePage()
             // Progress to next word
             currentWordNum++
         }
 
+    fun useButton1(view: View) {
+        val button = findViewById<Button>(R.id.letter1)
+        spellWord(button)
+    }
+
+    fun useButton2(view: View) {
+        val button = findViewById<Button>(R.id.letter2)
+        spellWord(button)
+    }
+
+    fun useButton3(view: View) {
+        val button = findViewById<Button>(R.id.letter3)
+        spellWord(button)
+    }
+
+    fun useButton4(view: View) {
+        val button = findViewById<Button>(R.id.letter4)
+        spellWord(button)
+
+    }
+
+    fun useButton5(view: View) {
+        val button = findViewById<Button>(R.id.letter5)
+        spellWord(button)
+    }
+
+    fun useButton6(view: View) {
+        val button = findViewById<Button>(R.id.letter6)
+        spellWord(button)
+    }
+
+    fun useButton7(view: View) {
+        val button = findViewById<Button>(R.id.letter7)
+        spellWord(button)
+    }
+
+    fun useButton8(view: View) {
+        val button = findViewById<Button>(R.id.letter8)
+        spellWord(button)
+    }
+
     // Take inputs from the buttons to spell out the word
-    fun spellWord(view: View) {
+    fun spellWord(button: Button) {
+        // Here we hold the display letters
+        val wordDisplay = findViewById<TextView>(R.id.answerDisplay)
+        // Save the value from the button
+        val buttonValue = button.text as String
         // Make sure the word isn't complete
+        // and check if the selection is correct
         if (currentLetterNum < currentWord.length)
-//            // Check it the selection is correct
-//            if (buttonValue == wordLetters[currentLetterNum])
-//                // Add the button value to the letter list
-//                currentLetters.add(buttonValue)
-            TODO("Figure out how to pass the button value")
+            if (buttonValue.compareTo(currentWord[currentLetterNum].toString()) == 0) {
+                this.currentLetters += buttonValue
+                button.text = ""
+                currentLetterNum++
+            }
+        wordDisplay.text = currentLetters
     }
 
     private fun initializePage() {
